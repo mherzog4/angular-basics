894c3f78c1fbf088a2eb296db5cf3bb8a65cd363
Mherzog <matthewherzog4@gmail.com>
2023-04-09 17:07:17 -0400

    transforming content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,20 @@ import { Component } from '@angular/core';
   // styles: ['']
 })
 export class AppComponent {
-  name = 'Matt'
+  name = 'matt herzog'
   imgUrl = 'https://angular.io/assets/images/logos/angular/angular.png'
+  images = ['https://angular.io/assets/images/logos/angular/angular.png', 'https://angular.io/assets/images/logos/angular/angular_solidBlack.png']
+  currentDate = new Date()
+  cost = 2000
+  temp = 25.3
+  pizza = {
+    toppings: 
+      ['pepperoni', 'sausage', 'mushrooms', 'onions', 'olives', 'pineapple'],
+      size: 'large'
+  }
+
+  blueClass = false
+  fontSize = 20
 
   getName() {
     return this.name
